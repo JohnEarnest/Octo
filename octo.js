4186708633e87b9002bdc4ba91cd0906aa16db1f
John Earnest <JohnEarnest@gmail.com>
2014-07-05 18:31:29 -0500

    added support for a complete range of comparison operators.

--- octo.js
+++ octo.js
@@ -385,11 +385,16 @@ function Compiler(source) {
 	this.conditional = function(negated) {
 		var reg   = this.register();
 		var token = this.next();
+		var compTemp = 0xE; // temporary reg for comparison operators
 		if (negated) {
 			if      (token == "=="  ) { token = "!="; }
 			else if (token == "!="  ) { token = "=="; }
 			else if (token == "key" ) { token == "-key"; }
 			else if (token == "-key") { token == "key"; }
+			else if (token == "<"   ) { token == ">="; }
+			else if (token == ">"   ) { token == "<="; }
+			else if (token == ">="  ) { token == "<"; }
+			else if (token == "<="  ) { token == ">"; }
 		}
 		if (token == "==") {
 			if (this.isRegister()) { this.inst(0x90 | reg, this.register() << 4); }
@@ -404,6 +409,30 @@ function Compiler(source) {
 		}
 		else if (token == "-key") {
 			this.inst(0xE0 | reg, 0x9E);
+		}
+		else if (token == ">") {
+			if (this.isRegister()) { this.fourop(0x8, compTemp, this.register(), 0x0); }
+			else                   { this.inst  (0x60 | compTemp, this.shortValue()); }
+			this.fourop(0x8, compTemp, reg, 0x5); // ve -= v1
+			this.inst(0x3F, 0);                   // if vf != 0 then ...
+		}
+		else if (token == "<") {
+			if (this.isRegister()) { this.fourop(0x8, compTemp, this.register(), 0x0); }
+			else                   { this.inst  (0x60 | compTemp, this.shortValue()); }
+			this.fourop(0x8, compTemp, reg, 0x7); // ve =- v1
+			this.inst(0x3F, 0);                   // if vf != 0 then ...
+		}
+		else if (token == ">=") {
+			if (this.isRegister()) { this.fourop(0x8, compTemp, this.register(), 0x0); }
+			else                   { this.inst  (0x60 | compTemp, this.shortValue()); }
+			this.fourop(0x8, compTemp, reg, 0x5); // ve -= v1
+			this.inst(0x4F, 0);                   // if vf == 0 then ...
+		}
+		else if (token == "<=") {
+			if (this.isRegister()) { this.fourop(0x8, compTemp, this.register(), 0x0); }
+			else                   { this.inst  (0x60 | compTemp, this.shortValue()); }
+			this.fourop(0x8, compTemp, reg, 0x7); // ve =- v1
+			this.inst(0x4F, 0);                   // if vf == 0 then ...
 		}
 		else {
 			throw "Conditional flag expected, got '" + token + "!";
