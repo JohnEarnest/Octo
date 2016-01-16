c1683ce3cd19df675df49039659a8b5594abb275
John Earnest <JohnEarnest@gmail.com>
2016-01-16 08:15:00 -0700

    in Safari, AudioContext.close() is not defined. Reworked setup and teardown code to reuse the AudioContext across runs.

--- js/shared.js
+++ js/shared.js
@@ -75,18 +75,18 @@ var audioSource;
 var audioData;
 
 var AudioBuffer = function(buffer, duration) {
-	if (!(this instanceof AudioBuffer))
-		return new AudioBuffer(buffer, duration)
+	if (!(this instanceof AudioBuffer)) {
+		return new AudioBuffer(buffer, duration);
+	}
 
-	this.pointer = 0
-	this.buffer = buffer
-	this.duration = duration
+	this.pointer = 0;
+	this.buffer = buffer;
+	this.duration = duration;
 }
 
 AudioBuffer.prototype.write = function(buffer, index, size) {
 	size = Math.max(0, Math.min(size, this.duration))
-	if (!size)
-		return size
+	if (!size) { return size; }
 
 	this.duration -= size;
 	var bufferSize = this.buffer.length;
@@ -101,7 +101,7 @@ AudioBuffer.prototype.write = function(buffer, index, size) {
 }
 
 AudioBuffer.prototype.dequeue = function(duration) {
-	this.duration -= duration
+	this.duration -= duration;
 }
 
 var FREQ = 4000;
@@ -110,16 +110,15 @@ var SAMPLES = 16;
 var BUFFER_SIZE = SAMPLES * 8
 
 function audioSetup() {
-	if (audio) { return; }
-
-	if (typeof AudioContext !== 'undefined') {
-		audio = new AudioContext();
+	if (!audio) {
+		if (typeof AudioContext !== 'undefined') {
+			audio = new AudioContext();
+		}
+		else if (typeof webkitAudioContext !== 'undefined') {
+			audio = new webkitAudioContext();
+		}
 	}
-	else if (typeof webkitAudioContext !== 'undefined') {
-		audio = new webkitAudioContext();
-	}
-
-	if (audio) {
+	if (audio && !audioNode) {
 		audioNode = audio.createScriptProcessor(4096, 1, 1);
 		audioNode.onaudioprocess = function(audioProcessingEvent) {
 			var outputBuffer = audioProcessingEvent.outputBuffer;
@@ -131,26 +130,29 @@ function audioSetup() {
 				var size = samples_n - index;
 				var written = audioData[0].write(outputData, index, size);
 				index += written;
-				if (written < size)
+				if (written < size) {
 					audioData.shift();
+				}
 			}
 
-			while(index < samples_n)
+			while(index < samples_n) {
 				outputData[index++] = 0;
+			}
 		}
-		audioData = []
+		audioData = [];
 		audioNode.connect(audio.destination);
-		return true
+		return true;
 	}
-	else
-		return false;
+	if (audio && audioNode) { return true; }
+	return false;
 }
 
 function stopAudio() {
 	if (!audio) { return; }
-	audioNode.disconnect(audio.destination)
-	audio.close()
-	audio = null
+	if (audioNode) {
+		audioNode.disconnect();
+		audioNode = null;
+	}
 	audioData = [];
 }
 
@@ -161,14 +163,15 @@ function playPattern(soundLength, buffer, remainingTicks) {
 
 	var samples = Math.floor(BUFFER_SIZE * audio.sampleRate / FREQ);
 	var audioBuffer = new Array(samples);
-	if (remainingTicks && audioData.length > 0)
+	if (remainingTicks && audioData.length > 0) {
 		audioData[audioData.length - 1].dequeue(Math.floor(remainingTicks * audio.sampleRate / TIMER_FREQ));
+	}
 
 	for(var i = 0; i < samples; ++i) {
 		var srcIndex = Math.floor(i * FREQ / audio.sampleRate);
 		var cell = srcIndex >> 3;
 		var bit = srcIndex & 7;
-		audioBuffer[i] = (buffer[srcIndex >> 3] & (0x80 >> bit))? VOLUME: 0;
+		audioBuffer[i] = (buffer[srcIndex >> 3] & (0x80 >> bit)) ? VOLUME: 0;
 	}
 	audioData.push(new AudioBuffer(audioBuffer, Math.floor(soundLength * audio.sampleRate / TIMER_FREQ)));
 }
@@ -177,4 +180,4 @@ function escapeHtml(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-};
+}
