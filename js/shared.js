JohnEarnest/Octo

commit ad757955c78e1156ca06848dd42d74b41d5783f3

Complete overhaul of the Octo web UI. Improvements include but are not limited to a better text editor based on CodeMirror with syntax highlighting, automatic saving/restoring of editor state, support for ludicrous speed, buttons for shifting/clearing in the sprite editor, a keyboard shortcut for compile+run, a quirks-mode setting for vblanking after sprite draws, configurable rom size limits based on target platform, compiler error messages including line number, and a feature for recording animated GIFs of running programs.

js/shared.js
@@ -9,27 +9,31 @@
 var scaleFactor = 5;
 var renderTarget = "target";
 
+const optionFlags = [
+	"tickrate",
+	"fillColor",
+	"fillColor2",
+	"blendColor",
+	"backgroundColor",
+	"buzzColor",
+	"quietColor",
+	"shiftQuirks",
+	"loadStoreQuirks",
+	"vfOrderQuirks",
+	"clipQuirks",
+	"vBlankQuirks",
+	"jumpQuirks",
+	"enableXO",
+	"screenRotation",
+]
 function unpackOptions(emulator, options) {
-	var flags = [
-		"tickrate",
-		"fillColor",
-		"fillColor2",
-		"blendColor",
-		"backgroundColor",
-		"buzzColor",
-		"quietColor",
-		"shiftQuirks",
-		"loadStoreQuirks",
-		"vfOrderQuirks",
-		"clipQuirks",
-		"jumpQuirks",
-		"enableXO",
-		"screenRotation",
-	]
-	for (var x = 0; x < flags.length; x++) {
-		var flag = flags[x];
-		if (flag in options) { emulator[flag] = options[flag]; }
-	}
+	optionFlags.forEach(x => { if (x in options) emulator[x] = options[x] })
+	emulator.enableXO = 1 // legacy option, force on
+}
+function packOptions(emulator) {
+	const r = {}
+	optionFlags.forEach(x => r[x] = emulator[x])
+	return r
 }
 
 function setRenderTarget(scale, canvas) {
@@ -48,14 +52,10 @@ function setRenderTarget(scale, canvas) {
 	if (emulator.screenRotation == 90 || emulator.screenRotation == 270) {
 		c.width  = h;
 		c.height = w;
-		c.style.marginLeft = hm;
-		c.style.marginTop  = wm;
 	}
 	else {
 		c.width  = w;
 		c.height = h;
-		c.style.marginLeft = wm;
-		c.style.marginTop  = hm;
 	}
 }
 

embed.html
@@ -2,8 +2,12 @@
 	<head>
 		<title>Octo Embeddable</title>
 	</head>
-	<body style="margin:0px; padding:0px;">
-		<canvas id="target" width="640" height="320" style="position: absolute; left: 50%; top: 50%;"></canvas>
+	<style>
+		body        { margin:0px;padding:0px; }
+		body canvas { width:100%;height:100%; }
+	</style>
+	<body>
+		<canvas id="target" width="640" height="320"></canvas>
 		<script src="js/compiler.js"></script>
 		<script src="js/emulator.js"></script>
 		<script src="js/shared.js"></script>

js/compiler.js
@@ -275,6 +275,7 @@ Compiler.prototype.reservedNames = {
 	"scroll-down":true, "scroll-right":true, "scroll-left":true,
 	"lores":true, "hires":true, "loadflags":true, "saveflags":true, "i":true,
 	"audio":true, "plane":true, "scroll-up":true, ":macro":true, ":calc":true, ":byte":true,
+	":call":true,
 };
 
 Compiler.prototype.checkName = function(name, kind) {
@@ -740,6 +741,9 @@ Compiler.prototype.instruction = function(token) {
 	}
 	else if (this.isRegister(token)) {
 		this.vassign(this.register(token), this.next());
+	}
+	else if (token == ":call") {
+		this.immediate(0x20, this.wideValue(this.next()));
 	}
 	else {
 		this.immediate(0x20, this.wideValue(token));

js/debugger.js
@@ -1,0 +1,124 @@
+/**
+* Debugger and Profiler
+**/
+
+const runContinue = document.getElementById('run-continue')
+const debugPanel  = document.getElementById('debugger')
+
+const regNumFormat = {}
+function cycleNumFormat(r) {
+	const f = ['hex','bin','dec','hex']
+	regNumFormat[r] = f[f.indexOf(regNumFormat[r] || 'hex')+1]
+	haltBreakpoint()
+}
+
+function getLabel(addr,reg,raw) {
+	var n = 'hex-font', x = 0
+	for (var k in emulator.metadata.labels) {
+		var v = emulator.metadata.labels[k]
+		if (v > x && v <= addr) { n = k; x = v }
+	}
+	if (raw) return n
+	return '('+n+(x == addr ? '' : ' + '+(addr-x))+')'
+}
+
+function dumpRegisters(showV) {
+	const line = (text, click) => '<span' + (click ? ' onClick="'+click+'"' : '')+'>' + text + '</span><br>'
+	const register = (n, v, f) => line(n + ' := ' + numericFormat(v, regNumFormat[n || 'hex']) + ' ' + f(v,n), 'cycleNumFormat(\''+n+'\')')
+	const aliases = (addr,reg) => {
+		var a = emulator.metadata.aliases
+		var r = +('0x'+reg.slice(1))
+		var n = Object.keys(a).filter(k => a[k] == r).join(', ')
+		return n.length ? '('+n+')' : ''
+	}
+	return (
+		line('tick count: ' + emulator.tickCounter) +
+		line('breakpoint: ' + name) +
+		register('pc', emulator.pc, getLabel) +
+		register('i', emulator.i, getLabel) +
+		(showV ? range(16).map(x => register('v'+(x.toString(16).toUpperCase()), emulator.v[x], aliases)).join('') : '') +
+		'<br>'
+	)
+}
+function dumpStack() {
+	return 'inferred stack trace:<br>' + emulator.r.map(x => hexFormat(x) + getLabel(x) + '<br>').join('') + '<br>'
+}
+function dumpContext() {
+	const dbg = emulator.metadata.dbginfo
+	const pcline = dbg.getLine(emulator.pc)
+	var memlo = emulator.pc, memhi = emulator.pc
+	while (dbg.getLine(memlo - 1) > pcline - 8) memlo--
+	while (dbg.getLine(memhi + 1) < pcline + 8) memhi++
+	var ind = memlo
+	const lines = []
+	for (var x = dbg.getLine(memlo); x <= dbg.getLine(memhi); x++) lines.push(x)
+	const row = (c,a,d,s) => '<tr'+(c?' class=\'current\'':'')+'><td>'+a+'</td><td>'+d+'</td><td><pre>'+escapeHtml(s)+'</pre></td></tr>'
+	const linebytes = x => { var r = ''; while(dbg.getLine(ind) == x) r += hexFormat(emulator.m[ind++]).slice(2) + ' '; return r }
+	return (
+		'context:<br><table class=\'debug-context\'>' +
+			row(false, 'addr', 'data', 'source') +
+			lines.filter(x => !dbg.lines[x].match(/^\s*$/)).map(x => {
+				const here = dbg.getLine(ind)
+				return row(
+					here == pcline,
+					here != x ? '' : hexFormat(ind).slice(2),
+					here != x ? '' : linebytes(x),
+					dbg.lines[x]
+				)
+			}).join('') +
+		'</table>'
+	)
+}
+function dumpProfile() {
+	const profile = []
+	for (var addr = 0; addr < 65536; ) {
+		while (emulator.profile_data[addr] == undefined && addr < 65536) addr++
+		if (addr > 65535) break
+
+		const head = addr, label = getLabel(addr, 0, true)
+		var ticks = 0
+		while (emulator.profile_data[addr] != undefined && getLabel(addr, 0, true) == label) {
+			ticks += emulator.profile_data[addr]
+			addr += 2
+		}
+		if (addr > 65535) break
+
+		profile.push({
+			ticks:   ticks,
+			percent: 100.0 * (ticks / emulator.tickCounter),
+			calls:   emulator.profile_data[head],
+			source:  getLabel(head) + ' + ' + ((addr - 2) - head),
+		})
+	}
+	return (
+		'<table class=\'debug-profile\'><tr> <td>ticks</td> <td>time</td> <td>calls</td> <td>source</td> </tr>'+
+			profile.sort((a,b) => b.percent - a.percent).slice(0, 20).map(
+				x => '<tr><td>'+x.ticks+'</td> <td>'+x.percent.toFixed(2)+'%</td> <td>'+x.calls+'</td> <td>'+x.source+'</td></tr>'
+			).join('') +
+		'</table>' +
+		'<div class=\'debug-profile-results\'>' +
+			'<div>Full results:<div>' +
+			'<div class=\'debug-profile-full\'>' + JSON.stringify(profile) + '</div>' +
+		'</div>'
+	)
+}
+
+function clearBreakpoint() {
+	setVisible(runContinue, false)
+	setVisible(debugPanel,  false)
+	emulator.breakpoint = false
+}
+
+function haltBreakpoint(name) {
+	setVisible(runContinue, true, 'inline')
+	setVisible(debugPanel,  true)
+	emulator.breakpoint = true
+	debugPanel.innerHTML = dumpRegisters(true) + dumpStack() + dumpContext()
+}
+
+function haltProfiler(breakName) {
+	setVisible(runContinue, true, 'inline')
+	setVisible(debugPanel,  true)
+	emulator.breakpoint = true
+	debugPanel.innerHTML = dumpRegisters(false) + dumpProfile()
+}

js/decompiler.js
@@ -58,7 +58,7 @@ function formatInstruction(a, nn) {
 	if (op == 0x00FE)           { return "lores"; } // schip
 	if (op == 0x00FF)           { return "hires"; } // schip
 	if (o == 0x1)               { return "jump " + name(lnames, nnn); }
-	if (o == 0x2)               { return name(snames, nnn); }
+	if (o == 0x2)               { return nnn in snames ? name(snames, nnn) : ':call ' + hexFormat(nnn); }
 	if (o == 0x3)               { return "if " + vx + " != " + numericFormat(nn) + " then"; }
 	if (o == 0x4)               { return "if " + vx + " == " + numericFormat(nn) + " then"; }
 	if (o == 0x5 && n == 0x0)   { return "if " + vx + " != " + vy + " then"; }

commit d459393d2254ff8446bc4446f3f384d02fbd3029

Fix screen clearing on lowres/hires transition.
This was broken in ad757955c78e, and presented in Mr Worm, where the
lores title screen wasn't cleared when starting hires gameplay:
http://johnearnest.github.io/Octo/index.html?gist=f3685a75817cde6d5c0d

js/shared.js
@@ -105,15 +105,14 @@ function renderDisplay(emulator) {
 	var c = document.getElementById(renderTarget);
 
 	// Canvas rendering can be expensive. Exit out early if nothing has changed.
-	// NOTE: toggling emulator.hires changes emulator.p dimensions.
-	var colors = [emulator.backColor, emulator.fillColor, emulator.fillColor2, emulator.blendColor];
+	var colors = [emulator.backgroundColor, emulator.fillColor, emulator.fillColor2, emulator.blendColor];
 	if (c.last !== undefined) {
 		if (arrayEqual(c.last.p[0], emulator.p[0]) && arrayEqual(c.last.p[1], emulator.p[1])
 				&& arrayEqual(c.last.colors, colors)) {
 			return;
 		}
-		if (c.last.p[0].length != emulator.p[0].length)
-			c.last = undefined
+		if (c.last.hires !== emulator.hires)
+			c.last = undefined;  // full redraw when switching resolution
 	}
 	var g = c.getContext("2d");
 	getTransform(emulator, g);
@@ -138,7 +137,8 @@ function renderDisplay(emulator) {
 
 	c.last = {
 		colors: colors,
-		p: [emulator.p[0].slice(), emulator.p[1].slice()]
+		p: [emulator.p[0].slice(), emulator.p[1].slice()],
+		hires: emulator.hires,
 	};
 }
 