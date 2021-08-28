6d03cc220d5780f0a94c895a5fa4980e292f8665
Kouzerumatsu / Bananafox <akbarkouzone@gmail.com>
2021-08-28 17:32:32 +0800

    a complete cleanup

--- js/shared.js
+++ js/shared.js
@@ -154,7 +154,7 @@ function renderDisplay(emulator) {
 var audio;
 var audioNode;
 var audioSource;
-var audioData = [];
+var audioData;
 
 var AudioBuffer = function(buffer, duration) {
 	if (!(this instanceof AudioBuffer)) {
@@ -188,13 +188,8 @@ AudioBuffer.prototype.dequeue = function(duration) {
 
 var FREQ = 4000;
 var TIMER_FREQ = 62.5;
-var SAMPLES = 128;
-var BUFFER_SIZE = SAMPLES * 8;
-
 var CHANNELS = 1;
 var PITCH_BIAS = 64;
-var DEFAULT_AUDIO_MODE = 0;
-
 
 function audioEnable() {
 	// this will only work if called directly from a user-generated input handler:
@@ -217,39 +212,32 @@ function audioSetup() {
 		audioNode.gain.gain.value = VOLUME ;
 		audioNode.onaudioprocess = function(audioProcessingEvent) {
 			var outputBuffer = audioProcessingEvent.outputBuffer;
-			var samples_n = outputBuffer.length;
-
 			var outputData = outputBuffer.getChannelData(0);
-			for(var ch=0;ch<CHANNELS;ch++){
-
-				var index = 0;
-				while(audioData[ch].length && index < samples_n) {
-					var size = samples_n - index;
-					var written = audioData[ch][0].write(outputData, index, size);
-					index += written;
-					if (written < size) {
-						audioData[ch].shift();
-					}
-				}
-
-				while(index < samples_n) {
-					outputData[index++] += 0;
-				}
-				//the last one can be long sound with high value of buzzer, so always keep it
-				if (audioData[ch].length > 1) {
-					var audioDataSize = 0;
-					var audioBufferSize = audioNode.bufferSize;
-					audioData[ch].forEach(function(buffer) { audioDataSize += buffer.duration; })
-					while(audioDataSize > audioBufferSize && audioData[ch].length > 1) {
-						audioDataSize -= audioData[ch].shift().duration;
-					}
+			var samples_n = outputBuffer.length;
+			var index = 0;
+			while(audioData.length && index < samples_n) {
+				var size = samples_n - index;
+				var written = audioData[0].write(outputData, index, size);
+				index += written;
+				if (written < size) {
+					audioData.shift();
 				}
 			}
 
+			while(index < samples_n) {
+				outputData[index++] += 0;
+			}
+			//the last one can be long sound with high value of buzzer, so always keep it
+			if (audioData.length > 1) {
+				var audioDataSize = 0;
+				var audioBufferSize = audioNode.bufferSize;
+				audioData.forEach(function(buffer) { audioDataSize += buffer.duration; })
+				while(audioDataSize > audioBufferSize && audioData.length > 1) {
+					audioDataSize -= audioData.shift().duration;
+				}
+			}
 		}
 		audioData = [];
-		for(var k=0; k<CHANNELS; k++)
-			audioData[k] = []
 		audioNode.connect(audioNode.gain);
 		audioNode.gain.connect(audio.destination);
 		return true;
@@ -269,38 +257,20 @@ function stopAudio() {
 
 var VOLUME = 0.25;
 
-function playPattern(soundLength, buffer, remainingTicks,  channel=0,
-	mode=DEFAULT_AUDIO_MODE,startPos=0,pitch=PITCH_BIAS,volume=1) {
+function playPattern(soundLength,buffer,remainingTicks,startPos=0,pitch=PITCH_BIAS) {
 	if (!audio) { return; }
 	audioEnable()
 	var freq = FREQ*2**((pitch-PITCH_BIAS)/48);
 	var samplesPerTick = audio.sampleRate / TIMER_FREQ;
 
-	/////////////////////////////////////////////////////////////////////
 	//  Samples extraction from buffer's bytes
+	var sampleData = new Uint8Array(buffer.length*8);
+	for(var i=0,z=0;i<sampleData.length;z=++z%buffer.length)
+		for (var cell = buffer[z],j=0;j<8;j++,cell&=255)
+			sampleData[i++]=(cell<<=1)&256?255:0
 
-	//  Each sample will be always normalized to byte range, suppose:
-	//  mode 0: 1-bit sample 0bA normalized to 0bAAAAAAAA
-	//  mode 1: 2-bit sample 0bAB normalized to 0bABABABAB
-	//  mode 2: 4-bit sample 0bABCD normalized to 0bABCDABCD
-	//  mode 3: 8-bit sample 0bABCDEFGH simply skips this process
-
-	var bits = 1 << mode;
-	var samplesPerByte = 8 >> mode;
-	var bitMasker = ( ( 1 << bits ) - 1 ) << 8;
-	var sampleData = new Uint8Array(buffer.length*samplesPerByte);
-	for(var cellAt=0,sampleAt=0;sampleAt<sampleData.length;cellAt=++cellAt%buffer.length){
-		var cellValue = buffer[cellAt];
-		for(var a=0;a<samplesPerByte;a++){
-			var sampleValue = ( ( cellValue << bits ) & bitMasker ) >> 8; 
-			for(var shifts=bits;shifts<8;shifts<<=1)
-				sampleValue = sampleValue << shifts | sampleValue
-			sampleData[sampleAt++] = sampleValue;
-			cellValue = cellValue << bits & 255;
-		}
-	}
-	if (remainingTicks && audioData[channel].length > 0)
-		audioData[channel][audioData[channel].length - 1].dequeue(Math.floor(remainingTicks * samplesPerTick));
+	if (remainingTicks && audioData.length > 0)
+		audioData[audioData.length - 1].dequeue(Math.floor(remainingTicks * samplesPerTick));
 	
 		
 	var samples = Math.ceil(samplesPerTick * soundLength);
@@ -308,34 +278,28 @@ function playPattern(soundLength, buffer, remainingTicks,  channel=0,
 	var audioBuffer = new Float32Array(samples);
 	var step = freq / audio.sampleRate, pos = startPos;
 	for(var i = 0, il = samples; i < il; i++) {
-		audioBuffer[i] = ( sampleData[Math.floor(pos)] / 255 - 0.5 ) * volume;
+		audioBuffer[i] = sampleData[Math.floor(pos)] / 255 - 0.5;
 		pos = ( pos + step ) % sampleData.length;
 	}
-	audioData[channel].push(new AudioBuffer(audioBuffer, Math.floor(soundLength * samplesPerTick)));
+	audioData.push(new AudioBuffer(audioBuffer, Math.floor(soundLength * samplesPerTick)));
 	
 	return pos;
 }
 
-function channelControl(channelTarget){
-	this.channel = channelTarget;
+function audioControl(){
 	this.position = 0;
 	this.reset = true;
 	this.buffer = [];
-	this.mode = 0;
 
 	this.timer = 0;
 	this.pitch = PITCH_BIAS;
-	this.pitchRamp = 0;
-	this.volume = 255;
-	this.volumeRamp = 0;
+	this.ramp = 0;
 
-	this.refresh =_=> {
+	this.refresh = _ => {
 		if (this.reset) { this.position = 0; this.reset = false; }
 		if (this.timer>0)
-			this.position = playPattern(1,this.buffer,0,this.channel,
-				this.mode,this.position,this.pitch,this.volume/255);
-		this.pitch=Math.min(Math.max(this.pitch+this.pitchRamp/4,0),255.75);
-		this.volume=Math.min(Math.max(this.volume+this.volumeRamp/4,0),255.75);
+			this.position = playPattern(1,this.buffer,0,this.position,this.pitch);
+		this.pitch=Math.min(Math.max(this.pitch+this.ramp/4,0),255.75);
 		this.timer-=this.timer>0;
 		if(this.timer == 0) this.reset = true;
 	}
@@ -343,55 +307,9 @@ function channelControl(channelTarget){
 		if(timer == 0) this.reset = true;
 		this.timer = timer;
 	}
-	this.setBuffer=(buffer,mode) => {
-		this.buffer = buffer;
-		this.mode = mode;
-	}
-	this.setPitch = (pitch) => (this.pitch = pitch);
-	this.setVolume = (volume) => this.volume = volume;
-	this.setPitchRamp = (ramp) => this.pitchRamp = ramp-256*(ramp>=128);
-	this.setVolumeRamp = (ramp) => this.volumeRamp = ramp-256*(ramp>=128);
-}
-
-function audioControl(){
-	this.channels = new Array(CHANNELS);
-	for(var k=0;k<this.channels.length;k++)
-		this.channels[k] = new channelControl(k);
-
-	this.refresh = _=>{
-		for(var k=0;k<this.channels.length;k++)
-			this.channels[k].refresh();
-	}
-
-	this.assignTimerSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setTimer;
-	}
-
-	this.assignBufferSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setBuffer;
-	}
-
-	this.assignPitchSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setPitch;
-	}
-
-	this.assignVolumeSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setVolume;
-	}
-
-	this.assignPitchRampSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setPitchRamp;
-	}
-
-	this.assignVolumeRampSetter = stubs=>{
-		for(var k=0;k<this.channels.length;k++)
-			stubs[k] = this.channels[k].setVolumeRamp;
-	}
+	this.setBuffer= buffer => this.buffer = buffer;
+	this.setPitch = pitch  => this.pitch = pitch;
+	this.setRamp = ramp => this.ramp = ramp-(ramp<<1&256);
 }
 
 function escapeHtml(str) {
