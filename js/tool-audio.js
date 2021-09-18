4f31ed82972872dcec3d79d0161b3aeedf746580
Kouzerumatsu / Bananafox <46141631+Kouzeru@users.noreply.github.com>
2021-09-19 00:46:04 +0800

    piano keys, and tone generator parameters

--- js/tool-audio.js
+++ js/tool-audio.js
@@ -2,6 +2,7 @@
 * Audio editor:
 **/
 
+const audioPianoKeys = document.getElementById('audio-piano-keys')
 const audioPatternEditor = textBox(document.getElementById('audio-pattern-editor'), false, '')
 const audioPatternCanvas = document.getElementById('audio-pattern-view')
 const audioToneCanvas = document.getElementById('audio-tone-view')
@@ -34,6 +35,16 @@ function shiftBytes(bytes, n) {
 	}
 	return r
 }
+function flipBytes(bytes, n){
+	return bytes.map(x => x ^ 0xFF)
+}
+
+function reverseBits(bytes, n){
+	return bytes.reverse().map(x =>
+		x>>7& 1|x>>5& 2|x>>3& 4|x>>1&  8|
+		x<<1&16|x<<3&32|x<<5&64|x<<7&128);
+}
+
 function playBytes(pattern, pitch){
 	if (audioSetup(emulator)) {
 		playPattern(0.5,pattern,undefined,undefined,pitch)
@@ -42,6 +53,47 @@ function playBytes(pattern, pitch){
 		document.getElementById('audio-error').innerHTML = 'Your browser does not support HTML5 Audio!'
 	}
 }
+
+/**
+*  Piano keys panel
+**/
+
+function drawPiano(pressed=-1){
+	const g = audioPianoKeys.getContext('2d')
+	const octaves = 5, keywidth = 8;
+	
+	g.fillStyle = emulator.backgroundColor;
+	g.fillRect(0,0,7*keywidth*octaves,32);
+
+	// render white keys
+	for (let key=0,keypos=0;key<octaves*7;key++,keypos+=keywidth){
+		g.fillStyle = Math.ceil(12*(Math.floor(key)-3)/7)+5 == pressed ?
+			emulator.blendColor : emulator.fillColor;
+		g.fillRect(keypos,0,keywidth-1,31)
+	}
+	// render black keys
+	for ( var m = 0; m < 2; m++ ){
+		for (let key=0,keypos=-1;key<octaves*5;key++,keypos += keywidth + 1){
+			if ( key%5 == 2 ) keypos += 6; if ( key%5 == 0 )  keypos += 5;
+			g.fillStyle = !m ? emulator.backgroundColor :
+				(Math.ceil(12*(key+1)/5)-2 != pressed)?
+				emulator.fillColor2:emulator.blendColor;
+			g.fillRect(keypos-m,-m,keywidth-2,20);
+		}
+	}
+}
+
+drawOnCanvas(audioPianoKeys, (x, y, draw) => {
+	const keywidth = 8;
+	// try to hover the piano keys
+	var k = x / keywidth + 0.5;
+	var press = !( y<20 && (k)%1<0.75 && 
+		( Math.floor(k)%7!=2 || Math.floor(k)%7!=6 ) )?
+		Math.ceil( 12 * ( Math.floor(k - 0.5 ) - 3 ) / 7 ) + 5:
+		Math.ceil( 12 * ( Math.floor(k) - 3 ) / 7 ) + 4;
+	audioPitch.value = press * 4 + 19;
+	updatePiano();
+})
 
 /**
 * Pattern panel
@@ -58,6 +110,14 @@ drawOnCanvas(audioPatternCanvas, (x, y, draw) => {
 })
 function audioPreview(){
 	playBytes(readPattern(audioPatternEditor), Math.max(0,Math.min(+audioPitch.value,255)))
+}
+document.getElementById('audio-reverse').onclick = _ => {
+	writePattern(audioPatternEditor, reverseBits(readPattern(audioPatternEditor)))
+	updateAudio()
+}
+document.getElementById('audio-invert').onclick = _ => {
+	writePattern(audioPatternEditor, flipBytes(readPattern(audioPatternEditor)))
+	updateAudio()
 }
 document.getElementById('audio-left').onclick = _ => {
 	writePattern(audioPatternEditor, shiftBytes(readPattern(audioPatternEditor), 1))
@@ -81,7 +141,6 @@ document.getElementById('audio-random').onclick = _ => {
 
 const toneDuty  = document.getElementById('audio-width')
 const tonePulse = document.getElementById('audio-pulse')
-const toneFreq  = document.getElementById('audio-freq')
 
 function audioBlend(){
 	return ({
@@ -92,21 +151,20 @@ function audioBlend(){
 	})[audioBlendMode.getValue()]
 }
 function audioTone(){
-	const duty=Math.max(0,Math.min(+toneDuty.value,255))
-	const freq=+toneFreq.value
-	const pulse=Math.ceil(tonePulse.value*duty)
+	const duty=128/Math.max(0,Math.min(+toneDuty.value,64))
+	const pulse=Math.ceil(tonePulse.value*duty/100)
 	const blend=audioBlend()
 	const pattern=readPattern(audioPatternEditor).map((old,i) => {
 		let r=0
 		for(let b=0;b<8;b++) r |= ((i*8+b)%duty<pulse?1:0)*(1<<(7-b))
 		return blend(old,r)
 	})
-	const pitch=0|Math.max(0,Math.min(255, Math.log2((freq*duty)/4000)*48+64))
+	const pitch=+audioPitch.value;
 	return {pattern,pitch}
 }
 toneDuty.onchange =toneDuty.onkeyup =updateAudio
 tonePulse.onchange=tonePulse.onkeyup=updateAudio
-toneFreq.onchange =toneFreq.onkeyup =updateAudio
+audioPitch.onchange = audioPitch.onkeyup = updatePiano;
 
 document.getElementById('audio-tone-preview').onclick = _ => {
 	const tone=audioTone()
@@ -129,5 +187,11 @@ function updateAudio() {
 	drawBytes(audioPatternCanvas, readPattern(audioPatternEditor))
 	const tone=audioTone()
 	drawBytes(audioToneCanvas, tone.pattern)
+	updatePiano()
 }
+
+function updatePiano() {
+	drawPiano(Math.floor((+audioPitch.value-16)/4),Math.floor((+audioPitch.value-16)/4));
+}
+
 audioPatternEditor.on('change', updateAudio)
